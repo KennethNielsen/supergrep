ebdba8f520cb9406d5bcc890a8a67c8b4855ad5f
Kenneth Nielsen <kni@spectroinlets.com>
2019-11-01 14:56:25 +0100

    Add classes for search results and pretty formatter for text results

--- src/supergrep.py
+++ src/supergrep.py
@@ -1,7 +1,19 @@
 """Main supergrep script"""
 
+import codecs
+import logging as LOG
+from typing import Optional, List
 from multiprocessing import Process, Queue, cpu_count, Pipe
+
+import colorama
+from colorama import Fore, Style
 import click
+import magic
+import attr
+
+
+LOG.basicConfig(level=LOG.ERROR)
+colorama.init()
 
 
 @click.command(
@@ -31,14 +43,14 @@ def search(pattern, paths, all_files, recursive):
         all_files (bool): Whether to search all files including spreadsheets
         recursive (bool): Whether to search recursively
     """
-    print("PATTERN", pattern)
-    print("PATHS", paths)
-    print("all_files", all_files)
-    print("recursive", recursive, end="\n\n")
+    LOG.info("Argument PATTERN  : %s", pattern)
+    LOG.info("Argument PATHS    : %s", paths)
+    LOG.info("Argument all_files: %s", all_files)
+    LOG.info("Argument recursive: %s", recursive)
 
     job_queue = Queue()
     worker_count = max(1, cpu_count() - 1)
-    workers = [SearchWorker(job_queue) for _ in range(worker_count)]
+    workers = [SearchWorker(job_queue, pattern) for _ in range(worker_count)]
     for worker in workers:
         worker.start()
 
@@ -53,7 +65,9 @@ def search(pattern, paths, all_files, recursive):
 
     for pipe in pipes:
         return_value = pipe.recv()
-        print("Parent got", return_value)
+        LOG.debug("Got result: %s", return_value)
+        if return_value:
+            return_value.print_output("pretty")
 
     for worker in workers:
         worker.join()
@@ -62,18 +76,79 @@ def search(pattern, paths, all_files, recursive):
 class SearchWorker(Process):
     """A work to search files in a separate process"""
 
-    def __init__(self, job_queue):
+    def __init__(self, job_queue, search_term):
         self.job_queue = job_queue
+        self.search_term = search_term
         super().__init__()
 
     def run(self):
         while True:
             job, pipe = self.job_queue.get()
-            print(self.name, "got", job)
+            LOG.debug("Worker %s got job: %s", self.name, job)
             if job is None:
                 break
-            pipe.send(job)
-        print(self.name, "Bye!")
+
+            result = self.search(job)
+            pipe.send(result)
+
+        LOG.info("Worker %s shutting down. Bye!", self.name)
+
+    def search(self, filepath):
+        with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
+            filetype = m.id_filename(filepath)
+
+        if filetype.startswith("text/"):
+            return self.search_txt(filetype, filepath)
+
+        LOG.info("%s, %s  ###  %s", type(filetype), filetype, filepath)
+
+    def search_txt(self, filetype, filepath):
+        """Return search result for a text file"""
+        # Lots more to do here for inferring encoding
+        with magic.Magic(flags=magic.MAGIC_MIME_ENCODING) as m:
+            encoding = m.id_filename(filepath)
+        LOG.info("Encoding: %s", encoding)
+
+        results = []
+        with codecs.open(filepath, encoding=encoding) as file_:
+            for line_no, line in enumerate(file_):
+                if self.search_term in line:
+                    results.append(SearchResult(filepath, line, line_no=line_no))
+
+        if results:
+            return SearchResults(results, self.search_term, rtype="text")
+        return None
+
+
+@attr.s(auto_attribs=True)
+class SearchResult:
+    """Class that represents a search result"""
+
+    filepath: str
+    rtext: str
+    line_no: Optional[int]
+
+
+@attr.s(auto_attribs=True)
+class SearchResults:
+    """Class that respresents all search results from a file"""
+
+    results: List[SearchResult]
+    search_term: str
+    rtype: str
+
+    def print_output(self, output_format):
+        getattr(self, f"print_{self.rtype}_{output_format}")()
+
+    def print_text_pretty(self):
+        for result in self.results:
+            text = result.rtext.rstrip().replace(
+                self.search_term, f"{Fore.RED}{self.search_term}{Fore.WHITE}"
+            )
+            print(
+                f"{Fore.MAGENTA}{result.filepath}, {Fore.GREEN}{result.line_no}: "
+                f"{Fore.WHITE}{Style.BRIGHT}{text}{Style.NORMAL}"
+            )
 
 
 if __name__ == "__main__":
